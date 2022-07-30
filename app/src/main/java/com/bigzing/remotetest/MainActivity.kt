239a840aaa31afeb15a70fac65deab726ead31f9
bigzing <bigzing@qq.com>
2022-07-30 20:16:19 +0800

    修改主文件

--- app/src/main/java/com/bigzing/remotetest/MainActivity.kt
+++ app/src/main/java/com/bigzing/remotetest/MainActivity.kt
@@ -2,8 +2,10 @@ package com.bigzing.remotetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    var data = 12
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
